70198b6427a61f4bf3639d157636374ab2675536
prxsss <9phuriphat.k@gmail.com>
2025-03-12 22:00:11 +0700

    Add PostListings widget for displaying a list of posts

---
+++ lib/presentation/widgets/post_listings.dart
@@ -0,0 +1,18 @@
+import 'package:blog_app/presentation/widgets/post_listing.dart';
+import 'package:flutter/material.dart';
+
+class PostListings extends StatelessWidget {
+  const PostListings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const PostListing();
+      },
+    );
+  }
+}
